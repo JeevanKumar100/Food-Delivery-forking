75995ecd98454972a23e2fd78303d45e1b2c3113
JeevanKumar100 <jeevankumarpadala1@gmail.com>
2025-11-28 12:25:58 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      process.env.MONGO_URL
-    )
-    .then(() =>console.log("DB Connected"));
+    .connect(process.env.MONGO_URI)   // <-- FIXED
+    .then(() => console.log("DB Connected"))
+    .catch((err) => {
+      console.error("DB Connection Error:", err.message);
+    });
 };
